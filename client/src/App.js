2daf0c119a151f0333f245065516f75e25f39b30
Jevgeni <dev.jedro1d@gmail.com>
2021-02-10 11:45:31 +0200

    todo list

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,107 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {useState} from "react";
+import {nanoid} from "nanoid";
+import Todo from "./components/Todo";
 import "./App.css";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
 
-function App() {
-	const [data, setData] = React.useState(null);
 
-	React.useEffect(() => {
-		fetch("/api")
-			.then((res) => res.json())
-			.then((data) => setData(data.message));
-	}, []);
+const FILTER_MAP = {
+	All: () => true,
+	Active: task => !task.completed,
+	Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+function App(props) {
+
+
+	function addTask(name) {
+		const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+		setTasks([...tasks, newTask]);
+	}
+
+	const [tasks, setTasks] = useState(props.tasks);
+	const taskList = tasks.map(task =>
+		<Todo
+			id={task.id}
+			name={task.name}
+			completed={task.completed}
+			key={task.id}
+			toggleTaskCompleted={toggleTaskCompleted}
+			editTask={editTask}
+			deleteTask={deleteTask}
+		/>);
+
+	const [filter, setFilter] = useState('All');
+	const filterList = FILTER_NAMES.map(name => (
+		<FilterButton
+			key={name}
+			name={name}
+			isPressed={name === filter}
+			setFilter={setFilter}
+		/>
+	));
+
+	const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+	const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+
+	function toggleTaskCompleted(id) {
+		const updatedTasks = tasks.map(task => {
+			// if this task has the same ID as the edited task
+			if (id === task.id) {
+				// use object spread to make a new object
+				// whose `completed` prop has been inverted
+				return {...task, completed: !task.completed}
+			}
+			return task;
+		});
+		setTasks(updatedTasks);
+	}
+
+	function editTask(id, newName) {
+		const editedTaskList = tasks.map(task => {
+			// if this task has the same ID as the edited task
+			if (id === task.id) {
+				//
+				return {...task, name: newName}
+			}
+			return task;
+		});
+		setTasks(editedTaskList);
+	}
+
+	function deleteTask(id) {
+		const remainingTasks = tasks.filter(task => id !== task.id);
+		setTasks(remainingTasks);
+	}
 
 	return (
-		<div>
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<p>{!data ? "Loading..." : data}</p>
-				</header>
+		<div className="todoapp stack-large">
+			<h1>TodoMatic</h1>
+			<Form addTask={addTask}/>
+
+			<div className="filters btn-group stack-exception">
+				{filterList}
 			</div>
-			<div>
-				<a
-					className="App-link"
-					href="http://liski.online"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Liski.Online
-				</a>
-			</div>
+
+			<h2 id="list-heading">
+				{headingText}
+			</h2>
+			<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading"
+			>
+				{taskList}
+			</ul>
 		</div>
-	);
+	)
 }
 
-
 export default App;
+
 
 
